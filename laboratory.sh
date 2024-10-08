0820b95404691672d5c6d20653a91a83fb6d9d91
Artemiadze <t0mi.v@yandex.ru>
2024-10-08 16:47:47 +0300

    add scripts with percentage

--- laboratory.sh
+++ laboratory.sh
@@ -1,32 +1,56 @@
 #!/bin/bash
-echo "сегодня " `date`
+
+# Создание папки фиксированного размера
+# dd if=/dev/zero of=~/lab_bash/Fixed_size.img bs=1M count=100
+# mkfs.ext4 ~/lab_bash/Fixed_size.img
+# cd lab_bash
+# sudo mount -o loop Fixed_size.img log 
+# sudo umount /home/lab_bash/log - размонтировка папки
+
+# Создание папки размером 1ГБ
+LOG_DIR="log"
+if [ ! -d "$LOG_DIR" ]; then
+    sudo mkdir -p log
+    sudo dd if=/dev/zero of=log.img bs=1M count=512
+    sudo mkfs.ext4 log.img
+    sudo mount -o loop log.img log
+
+    # Перекидывание тестовых файлов для работы этого скрипта
+    sudo cp 1.txt log
+    sudo cp 2.txt log
+    sudo cp 3.txt log
+    sudo cp 4.txt log
+    sudo cp 5.txt log
+    sudo cp sixth.txt log
+else
+    echo -e "\n Папка уже создана"
+fi
 
 echo -e "\n введите путь к директории и заполненость:"
 read path_to
-read percentage
+read PERSENTAGE
 
 echo -e "\n ваша директория содержит файлы и папки:"
 ls $path_to
 
-# Получаем размер целевой папки и родительской папки
-TARGET_SIZE=$(du -sb "$path_to" | cut -f1)
-PARENT_DIR=$(dirname "$path_to")
-PARENT_SIZE=$(du -sb "$PARENT_DIR" | cut -f1)
 
-# Проверяем, что размер родительской папки не равен нулю
-if [ "$PARENT_SIZE" -eq 0 ]; then
-    echo "Ошибка: Размер родительской папки равен нулю."
-    exit 1
-fi
+# ИЗ-ЗА ЭТОГО МЕНЯ ЗАБЛОКИРОВАЛ ЛИНУКС
+#Чтобы сохранить монтирование после перезагрузки, вы можете добавить запись в файл /etc/fstab:
+# sudo echo "tmpfs /lab_bash/log tmpfs size=1G 0 0" >> /etc/fstab
+# или
+# sudo echo "/lab_bash/log.img /lab_bash/log ext4 loop 0 0" >> /etc/fstab
+#Это позволит автоматически смонтировать файловую систему при запуске системы.
+# Если вылезает с ошибка с доступом
+# sudo sh -c "echo 'log.img /log ext4 loop 0 0' >> /etc/fstab"
 
-# Рассчитываем процент
-PERCENTAGE_FOLDER=$(echo "scale=2; ($TARGET_SIZE / $PARENT_SIZE) * 100" | bc)
+# Извлекаем процент использования
+size=$(df "$path_to" | awk 'NR==2 {print $5}')
 
 # Выводим результат
-echo "Размер '${path_to}' составляет ${PERCENTAGE_FOLDER}% от размера родительской папки '${PARENT_DIR}'."
+echo "Процент использования папки '$path_to': $size"
 
 # Создаем бекап папку, если ее нет
-if [ $PERCENTAGE_FOLDER \> $percentage ]; then
+if [ $size \> $PERSENTAGE ]; then
     BACKUP_DIR="backup"
     if [ ! -d "$BACKUP_DIR" ]; then
         sudo mkdir "$BACKUP_DIR"
@@ -50,8 +74,10 @@ if [ $PERCENTAGE_FOLDER \> $percentage ]; then
 
     # Удаляем файлы из директории LOG_DIR
     for file in "${FILES[@]}"; do
-        rm "$path_to/$file"
+        sudo rm "$path_to/$file"
     done
 
     echo "Архивация завершена. Архив сохранен как $BACKUP_DIR/old_files_backup.tar.gz и файлы удалены из $path_to."
 fi
+
+sudo rm -R log
